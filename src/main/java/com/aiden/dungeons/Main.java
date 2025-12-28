ff701f308789145fdaefe0d25b06f993e11d69f0
Aiden8115 <3967083449@qq.com>
2025-12-28 08:42:06 +0800

    re-add the JavaFX dependency

--- src/main/java/com/aiden/dungeons/Main.java
+++ src/main/java/com/aiden/dungeons/Main.java
@@ -1,4 +1,15 @@
 package com.aiden.dungeons;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
