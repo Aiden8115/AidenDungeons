daf177c60adc66886f169d0641f439f3435b2f2b
Aiden8115 <3967083449@qq.com>
2025-12-28 12:29:36 +0800

    sets the JavaFX env

--- src/main/java/com/aiden/dungeons/Main.java
+++ src/main/java/com/aiden/dungeons/Main.java
@@ -1,15 +1,26 @@
 package com.aiden.dungeons;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public Scene scene;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setTitle("Aiden's Dungeons");
+        StackPane root = new StackPane();
 
+        this.scene = new Scene(root, 1000, 618);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ src/main/java/com/aiden/dungeons/render/Renderer.java
@@ -0,0 +1,4 @@
+package com.aiden.dungeons.render;
+
+public class Renderer {
+}
